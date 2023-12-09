082f3015d5b24dbcf5f7fb470fa93df870ec734b
lourainep <polouraine@gmail.com>
2023-12-10 00:01:16 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+const App = () => {
+  
+
+  return (
+    <>
+    <div className="container mx-auto bg-red p-4  w-[400px] border border-black rounded flex flex-col items-center">
+      <div className="text-3xl font-bold mb-4">To-Do App</div>
+    </div>
+    </>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        colors: {
+            white: "#FFFFFF",
+            red: "#900C3F",
+            gray: "#949191",
+        },
+        extend: {},
+    },
+    plugins: [],
+}
